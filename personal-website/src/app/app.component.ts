9d60197ecd1a4a088c6bca25759659d29d5037a7
ICARUS-2 <ebriffett01@gmail.com>
2022-06-21 08:51:56 -0400

    Fixed router scroll to top issue

--- personal-website/src/app/app.component.ts
+++ personal-website/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'personal-website';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+
+    //scrollPositionRestoration is buggy, this does it automatically
+    this.router.events.subscribe((evt) => {
+        if (!(evt instanceof NavigationEnd)) {
+            return;
+        }
+        let navbar = document.querySelector("#navbar")
+
+        navbar?.scrollIntoView();
+    });
+}
+
 }
